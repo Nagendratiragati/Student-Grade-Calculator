6f30160b2fab0a4259771c393175c203b72d9a1e
NAGENDRA <nagendratiragati44@gmail.com>
2025-08-15 15:37:23 +0530

    Create GradeCalculator.java

---
+++ GradeCalculator.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class GradeCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Student Name: ");
+        String name = sc.nextLine();
+
+        int total = 0;
+        int subjects = 5;
+
+        System.out.println("Enter marks for " + subjects + " subjects:");
+        for (int i = 1; i <= subjects; i++) {
+            System.out.print("Subject " + i + ": ");
+            int marks = sc.nextInt();
+            total += marks;
+        }
+
+        double percentage = total / (double) subjects;
+        char grade;
+
+        if (percentage >= 90) {
+            grade = 'A';
+        } else if (percentage >= 80) {
+            grade = 'B';
+        } else if (percentage >= 70) {
+            grade = 'C';
+        } else if (percentage >= 60) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
+
+        System.out.println("\n----- Result -----");
+        System.out.println("Name: " + name);
+        System.out.println("Total Marks: " + total);
+        System.out.println("Percentage: " + percentage + "%");
+        System.out.println("Grade: " + grade);
+
+        sc.close();
+    }
+}
